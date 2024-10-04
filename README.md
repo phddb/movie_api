8ea82b0ae18f1e51cded2e8643c2706d0f617b1d
d-H- <davehbenson@gmail.com>
2024-10-03 22:06:39 -0400

    post-interview refinements: fix bugs, add styling

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+This is a toy Rails app that displays a list of movies fetched from `https://developer.themoviedb.org/`. The heavy lifting is done in `app/controllers/movies_controller.rb`.
 
-Things you may want to cover:
+To run it:
+1. clone this repo
+2. install dependencies with `bundle install`
+3. launch the server with `bundle exec rails s`
+4. visit [http://localhost:3000/](http://localhost:3000/)
 
-* Ruby version
+#### Screenshot
 
-* System dependencies
+<img width="785" alt="screenshot" src="https://github.com/user-attachments/assets/853b57b5-6f36-4d07-a553-f45b404c6c80">
 
-* Configuration
+#### Movie
 
-* Database creation
+https://github.com/user-attachments/assets/a1e183e6-d7fa-45fc-bb78-fa2ef099562c
 
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,30 +1,34 @@
 class MoviesController < ApplicationController
 
-  # GET /movies or /movies.json
+  # GET /movies 
   def index
     #@movies = Movie.all
 
     puts "params: #{params}"
 
-    query = params["query"]
+    @query = params["query"] || "star wars"
+    @page = params["page"] || 1
+
 
     base_url = "https://api.themoviedb.org/3/search/movie"
+
+    # this API key probably shouldn't be committed to github.
+    # If this weren't a toy example I'd put it somewhere else.
     api_key = "a99cc60fc2b34dbb18cb806b8a88ed14"
-    page = params["page"]
 
-    resp = HTTParty.get("#{base_url}?api_key=#{api_key}&query=#{query}&page=#{page}")
 
+    # this call should probably have some error handling logic around it. 
+    #  it would be nice to display a sensible message to the user if 
+    #  api.themoviedb.org happens to be offline or something.
+    resp = HTTParty.get("#{base_url}?api_key=#{api_key}&query=#{@query}&page=#{@page}")
+    
     @movies = JSON.parse(resp.body)["results"]#.each{|mov| puts mov["title"]}
    
     @movies.each do |movie| 
       poster_path = movie["poster_path"]
-      movie["image_url"] = image_url = "https://image.tmdb.org/t/p/w500#{poster_path}"
+      movie["image_url"] = image_url = "https://image.tmdb.org/t/p/w300#{poster_path}"
     end
 
-  end
-
-  # GET /movies/1 or /movies/1.json
-  def show
   end
 
 end
